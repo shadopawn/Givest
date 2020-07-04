635f06b4025607ce61f217a453e716ed1f088195
Daniel Litt <Daniel_litt@comcast.net>
2020-07-03 23:31:02 -0400

    more css and refactoring

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import GivestTitle from './component/GivestTitle';
 import Leaderboard from './component/Leaderboard';
-import DonateButton from './component/DonateButton';
+import DonateDiv from './component/DonateDiv';
 
 function App() {
   return (
     <div className="App">
       <GivestTitle />
       <Leaderboard />
-      <DonateButton />
+      <DonateDiv />
     </div>
   );
 }
--- src/component/DonateDiv.js
+++ src/component/DonateDiv.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DonateButton() {
     return (
-        <div className="DonateButton">
+        <div className="DonateDiv">
             <h4>Start competeing</h4>
             <button type="button">Donate now</button>
         </div>
