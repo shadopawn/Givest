360ea5379a084bfd98f75858dd4ce2ce7089d107
Daniel Litt <Daniel_litt@comcast.net>
2020-07-09 21:31:02 -0400

    add cleanup to tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from '../App';
+
+afterEach(cleanup)
 
 test('renders app title', () => {
   const { getByText } = render(<App />);
--- src/__tests__/Leaderboard.test.js
+++ src/__tests__/Leaderboard.test.js
@@ -1,7 +1,8 @@
-import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import Leaderboard from "../components/Leaderboard";
+
+afterEach(cleanup)
 
 test("renders Leaderboard rows", () => {
   const testRows = [
