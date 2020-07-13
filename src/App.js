23431b29c6e9a20c81cbfb8deea401b340b6848c
Daniel Litt <Daniel_litt@comcast.net>
2020-07-13 14:52:25 -0400

    remove unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import GivestTitle from './components/GivestTitle';
 import Leaderboard from './components/Leaderboard';
