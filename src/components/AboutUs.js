9b32c027acd88b15171533a8f0a236cad67d5a6e
Daniel Litt <Daniel_litt@comcast.net>
2020-07-24 22:57:19 -0400

    add headings to about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,7 +6,7 @@ export default function AboutUs() {
             <h3>What is Givest</h3>
             <h3>How we process donations</h3>
             <h3>Total donations</h3>
-            <h3>About the charities</h3>
+            <h3>Charities</h3>
         </div>
     )
 }
