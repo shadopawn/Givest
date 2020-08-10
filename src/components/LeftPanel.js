1eea80778a0bcb4b05a50dd2ea24590bfe130f59
Daniel Litt <Daniel_litt@comcast.net>
2020-08-10 16:11:28 -0400

    add heading

--- src/components/LeftPanel.js
+++ src/components/LeftPanel.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './LeftPanel.css';
 
 export default function LeftPanel() {
     return (
         <div className="LeftPanel">
-            
+            <h1>Welcome to Givest</h1>
+            <h4>About us</h4>   
         </div>
     )
 }
--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './RightPanel.css';
+
 
 export default function RightPanel() {
     return (
         <div className="RightPanel">
-            
+            <h1>Top donations</h1>
         </div>
     )
 }
