a3d9df9f5f5b8130048ffa521faf2d21a92b6f97
Daniel Litt <Daniel_litt@comcast.net>
2020-08-12 17:19:46 -0400

    start table redesign

--- src/components/LeaderboardRow.js
+++ src/components/LeaderboardRow.js
@@ -3,14 +3,12 @@ import React from 'react'
 export default function LeaderboardRow({row, position}) {
     
     return (
-        <tbody>
-            <tr data-testid="leaderboard-row">
-                <td>{position}</td>
-                <td>${row.donationAmount.toFixed(2)}</td>
-                <td>{row.name}</td>
-                <td>{row.charity}</td>
-                <td><a href={row.customLink} target="_blank" rel="noopener noreferrer">Link</a></td>
-            </tr>
-        </tbody>
+        <tr data-testid="leaderboard-row">
+            <td>{position}</td>
+            <td>${row.donationAmount.toFixed(2)}</td>
+            <td>{row.name}</td>
+            <td>{row.charity}</td>
+            <td><a href={row.customLink} target="_blank" rel="noopener noreferrer">Link</a></td>
+        </tr>
     )
 }
