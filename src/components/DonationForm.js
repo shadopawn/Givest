cc4aaa85af9c2c69454332142b20bf169e9a6b66
Daniel Litt <Daniel_litt@comcast.net>
2020-08-25 22:00:15 -0400

    comment out useState

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React/*, { useState }*/ from 'react'
 import PropTypes from 'prop-types'
 import { charityList } from '../charityInfo';
 import SelectOption from './SelectOption';
